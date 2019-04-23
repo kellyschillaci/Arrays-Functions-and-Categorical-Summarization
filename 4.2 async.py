3665d55577c2593b22d802e5cfced0eb0e53f19e
Kelly Schillaci <43831968+kellyschillaci@users.noreply.github.com>
2019-04-23 14:45:31 -0400

    Add files via upload

---
+++ 4.2 async.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 11:35:24 2019
+
+@author: kdoyl
+"""
+
+def fred():
+    print("Zap")
+def jane():
+    print("ABC")
+    
+jane()
+fred()
+jane()
